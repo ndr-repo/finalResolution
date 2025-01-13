3a148b8635feacf58e39a1e0f50ebaa61e4ae8c2
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2025-01-12 22:49:12 -0500

    Added cfUagTest

--- Validators/cfUagTest.ps1
+++ Validators/cfUagTest.ps1
@@ -1,5 +1,7 @@
 Write-Host " "
-Invoke-RestMethod  -Method Get -Uri https://one.one.one.one/cdn-cgi/trace
+$uag = Read-Host "Enter the User-Agent to test through Cloudflare"
+Write-Host " "
+Invoke-RestMethod  -Method Get -Uri https://one.one.one.one/cdn-cgi/trace -UserAgent "$uag"
 Write-Host "finalResolution v1.0.0" -ForegroundColor Red
 Write-Host "Created by Gabriel H. @weekndr_sec" -ForegroundColor Red
 Write-Host "https://github.com/ndr-repo | http://weekndr.me" -ForegroundColor Red
