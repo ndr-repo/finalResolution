a926b656b643f367ec5cc1120fe12a8dc4950c7f
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2025-01-20 12:12:31 -0500

    Update psWayback.ps1

--- Web/psWayback.ps1
+++ Web/psWayback.ps1
@@ -12,9 +12,7 @@ $targetDomain = Read-Host "Enter the target domain"
 Write-Host "Gathering indexed URLs for $targetdomain..." -ForegroundColor Red
 Write-Host " "
 $resultsFile = "waybackurls_$targetDomain.txt"
-Invoke-RestMethod -Method Get -Uri "http://web.archive.org/cdx/search/cdx?url=$targetDomain/*&output=json&fl=original&collapse=urlkey" | Select-Object -ExpandProperty SyncRoot | grep -o -P "^http.*" | Out-File $resultsFile
-Write-Host " "
-# Get-Content $resultsFile | Sort-Object -Descending | more
+Invoke-RestMethod -Method Get -Uri "http://web.archive.org/cdx/search/cdx?url=$targetDomain/*&output=json&fl=original&collapse=urlkey" | Select-Object -ExpandProperty SyncRoot | grep -o -P "^http.*" | Out-File $resultsFile -Encoding utf8 -Force
 Write-Host " "
 Write-Host "Output saved to $resultsFile" -ForegroundColor Red
 Write-Host " "
